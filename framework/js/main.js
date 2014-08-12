78b4ab27ff125deba74e8e551880f422dcec249b
Ivan Ryabov <chiba.link@gmail.com>
2014-08-12 16:49:26 +0400

    test

--- framework/js/main.js
+++ framework/js/main.js
@@ -1,64 +1,29 @@
-function demoBoxResize() {
-    
-}
-
 jQuery(document).ready(function() {
 
     var resolutions = jQuery('.resolutions'),
         res = resolutions.find('li'),
-        box = jQuery('.demo-box');
+        box = jQuery('.demo-box'),
+        frame = box.find('.demo-iframe');
 
     res.on('click', function() {
         curRes = jQuery(this);
         
         res.each(function() {
-            // if (!curRes) {
-                jQuery(this).removeClass('active');
-            // }
+            jQuery(this).removeClass('active');
         });
 
         curRes.addClass('active');
         box.animate({
             width: jQuery(this).attr('data-res')
-        }, 400);
+        }, 400, function() {
+            var demoSrc = frame.src;
+            frame.src = '';
+            frame.src = demoSrc;
+
+            box.animate({
+                height: frame.contents().find('body').height()
+            }, 400);
+        });
     });
-
-    /*jQuery(document).on('mousedown', '.grab-me', function(event) {
-        var grabMe = jQuery(this),
-            wrap = grabMe.siblings('.demo-wrap'),
-            box = grabMe.parent('.demo-box'),
-            start = event.pageX,
-            cur, diff,
-            wrapWidth = wrap.width(),
-            grabMeX = parseInt(grabMe.css('right')),
-            proceed;
-
-        proceed = true;
-
-        console.log(proceed);
-        console.log(wrapWidth);
-
-        box.on('mousemove', function(event) {
-            if (proceed) {
-                cur = event.pageX;
-                diff = cur - start;
-
-                wrap.width(wrapWidth + diff);
-                grabMe.css('right', grabMeX - diff + 'px');
-            }
-        });
-
-        box.on('mouseleave', function() {
-            proceed = false;
-            console.log(proceed);
-        });
-
-        jQuery(document).on('mouseup', function() {
-            proceed = false;
-            console.log(proceed);
-        });
-
-        return false;
-    });*/
 
 });
